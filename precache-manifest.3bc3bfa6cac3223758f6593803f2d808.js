2834cdff3c7fb5172ecf1670cc8c5eedc66121b9
elia r <erahimi117@gmail.com>
2020-03-16 13:20:15 +0330

    Updates

---
+++ precache-manifest.3bc3bfa6cac3223758f6593803f2d808.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "33e9213ca1af0aa568e6559139258d81",
+    "url": "/ToDo/index.html"
+  },
+  {
+    "revision": "242ba72ba882663c3ce1",
+    "url": "/ToDo/static/css/main.2650203f.chunk.css"
+  },
+  {
+    "revision": "e1aac1576f889fb530e2",
+    "url": "/ToDo/static/js/2.6ef21e24.chunk.js"
+  },
+  {
+    "revision": "de1cedeacf4641d6277b42334cfacb1d",
+    "url": "/ToDo/static/js/2.6ef21e24.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "242ba72ba882663c3ce1",
+    "url": "/ToDo/static/js/main.a9c1c846.chunk.js"
+  },
+  {
+    "revision": "023724003694914063c7",
+    "url": "/ToDo/static/js/runtime-main.c4f969a1.js"
+  }
+]);
